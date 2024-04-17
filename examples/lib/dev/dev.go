ffc76e31a35573c8c94d8c84567e1079bc982ef8
Martim-Sensefinity <martim.mourao@sensefinity.com>
2024-04-17 09:49:28 +0100

    Update import statements to use sensefinitycloud/go-ble package

--- examples/lib/dev/dev.go
+++ examples/lib/dev/dev.go
@@ -1,8 +1,6 @@
 package dev
 
-import (
-	"github.com/sensefinitycloud/go-ble/ble"
-)
+import "github.com/sensefinitycloud/go-ble"
 
 // NewDevice ...
 func NewDevice(impl string, opts ...ble.Option) (d ble.Device, err error) {
