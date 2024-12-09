ab85a2cd17977d5411709c21e01145d6e8f6bd5b
Martim-Sensefinity <martim.mourao@sensefinity.com>
2024-12-09 13:42:42 +0000

    Handle invalid connection gracefully in ACL packet processing

--- linux/hci/hci.go
+++ linux/hci/hci.go
@@ -363,7 +363,8 @@ func (h *HCI) handleACL(b []byte) error {
 	h.muConns.Unlock()
 	if !ok {
 		_ = logger.Warn("invalid connection handle on ACL packet", "handle", handle)
-		panic("invalid connection handle on ACL packet")
+		h.conns[handle].Close()
+		return nil
 	}
 	c.chInPkt <- b
 	return nil
